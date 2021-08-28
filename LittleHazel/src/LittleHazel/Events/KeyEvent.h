a2492fcaf7bba7f4cd0daa45f2b4bad94b0f4d14
Easi-GH <easi.guo@outlook.com>
2021-08-28 14:07:51 +0800

    update

---
+++ LittleHazel/src/LittleHazel/Events/KeyEvent.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Event.h"
+
+namespace LH {
+
+	class LH_API KeyEvent : public Event
+	{
+
+	};
+}
--- LittleHazel/src/LittleHazel/Events/MouseEvent.h
+++ LittleHazel/src/LittleHazel/Events/MouseEvent.h
@@ -1,6 +1,25 @@
 #pragma once
+#include "Event.h"
+#include <sstream>
 
 namespace LH {
 
-	class LH_API MouseMovedEvent : public event
+	class LH_API MouseMovedEvent : public Event
+	{
+	public:
+		MouseMovedEvent(float x, float y)
+			:m_MouseX(x), m_MouseY(y) {}
+
+		inline float GetX() const { return m_MouseX; }
+		inline float GetY() const { return m_MouseY; }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "MouseMovedEvent" << m_MouseX << "," << m_MouseY;
+			return ss.str();
+		}
+	private:
+		float m_MouseX, m_MouseY;
+	};
 }
